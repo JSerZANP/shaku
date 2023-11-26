1d69738619090e45a7a2fb967f9208226fe31906
JSer <69352453+JSerZANP@users.noreply.github.com>
2023-11-26 22:45:48 +0900

    update

--- packages/remark-shaku-code-annotate/vitest.config.ts
+++ packages/remark-shaku-code-annotate/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 /// <reference types="vitest" />
 export default defineConfig({
   test: {
-    testTimeout: 1000 * 60 * 5,
+    testTimeout: 1000 * 60 * 20,
   },
 });
