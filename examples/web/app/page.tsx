932d396724084ec248a74aca8bcc3a70262eec13
JSer <69352453+JSerZANP@users.noreply.github.com>
2023-11-30 00:35:12 +0900

    add styling

--- examples/web/app/page.tsx
+++ examples/web/app/page.tsx
@@ -8,13 +8,23 @@ export default async function Page({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const codeFromParams = searchParams?.code;
   return (
     <div className={styles.preview}>
       <Head>
         <title>Shaku</title>
       </Head>
-      <$.div $maxWidth={690} $margin="0 auto" $padding={"3rem 2rem"}>
+      <$.div $maxWidth={690} $margin="0 auto" $padding={"0 2rem 2rem"}>
+        <$.nav
+          $display="flex"
+          $justifyContent="center"
+          $padding="1rem"
+          $gap="1rem"
+          $fontSize={"14px"}
+          $borderBottom={"1px solid var(--color-line-3rd)"}
+        >
+          <a href="/playground">Shaku Playground</a>
+          <a href="/snippet">Shaku Snippet</a>
+        </$.nav>
         <$.h1 $textAlign="center">Shaku - code annotation made easy</$.h1>
 
         <p>
@@ -69,9 +79,7 @@ export default async function Page({
             <a href="https://github.com/markedjs/marked">marked</a>
           </$.li>
         </$.ol>
-        <$.p $lineHeight="1.5" $marginTop={"2rem"}>
-          Also some demos you can refer to:
-        </$.p>
+        <$.p>Also some demos you can refer to:</$.p>
         <$.ol $listStyle="disc" $lineHeight="1.5">
           <$.li $margin="8px 0">
             <a href="https://stackblitz.com/edit/github-yunziv?file=src%2Fcontent%2Fblog%2Fshaku.mdx">
@@ -85,14 +93,26 @@ export default async function Page({
           </$.li>
         </$.ol>
 
-        <$.p $lineHeight="1.5" $marginTop={"2rem"}>
-          Or you can just inspect the source code of this website TODO
+        <$.p>Or you can just inspect the source code of this website TODO</$.p>
+        <$.h2 $textAlign="center">Supported Languages</$.h2>
+        <$.p>
+          Shaku supports most languages that are supported by Shiki. You can
+          find the +150 languages from <a href="/snippet">Shaku Snippet</a>.
         </$.p>
-
         <$.h2 $textAlign="center">Styling</$.h2>
         <p>
-          Shaku renders code into HTML with class names prefixed with{" "}
-          <code>shaku</code>.
+          Shaku renders code into a <code>&lt;pre /&gt;</code> with class
+          <code>.shaku</code>, and shaku elements have class names prefixed with{" "}
+          <code>.shaku</code>, you can use the{" "}
+          <a href="https://github.com/JSerZANP/shaku/blob/main/examples/web/css/shaku.css">
+            {" "}
+            CSS from this website
+          </a>{" "}
+          and adapt to your needs.
+        </p>
+        <p>
+          The class names for each Shaku element will be explained in Syntax
+          section.
         </p>
 
         <$.h2 $textAlign="center">Syntax</$.h2>
@@ -114,8 +134,7 @@ export default async function Page({
         />
         <$.p>
           Place <code>^</code> for the arrow, and <code>[]</code> for the text,
-          you can also use your own markdown parser to support basic markdown
-          syntax.
+          you can also enable basic markdown support.
         </$.p>
         <CodeBlock
           $marginTop={"1rem"}
@@ -125,6 +144,33 @@ export default async function Page({
 //       [Check it out! [jser.dev](https://jser.dev)]
 // This is a normal comment`}
           lang="js"
+        />
+        <$.p>
+          <code>.shaku-callout</code> and <code>.shaku-callout-arrow</code> are
+          used to style callout.
+        </$.p>
+        <CodeBlock
+          $marginTop={"1rem"}
+          code={`.shaku-callout {
+  background-color: var(--color-shaku-callout-light, #0685ce);
+  color: #fff;
+  padding: 0.5em 1ch;
+  position: relative;
+  margin: 0.5em 0 0 -0.2ch;
+  display: inline-block;
+  border-radius: 2px;
+}
+.shaku-callout-arrow {
+  width: 1ch;
+  height: 1ch;
+  display: inline-block;
+  background-color: var(--color-shaku-callout-light, #0685ce);
+  position: absolute;
+  top: -0.5ch;
+  transform: rotate(45deg);
+  margin-left: 0.2ch;
+}`}
+          lang="css"
         />
         <$.h3>Underlines</$.h3>
 
@@ -169,6 +215,51 @@ const blog = "jser.dev"
           lang="js"
         />
 
+        <$.p>
+          <code>.shaku-underline</code> is the base style,{" "}
+          <code>.shaku-underline-wavy</code>,{" "}
+          <code>.shaku-underline-solid</code>
+          and <code>.shaku-underline-dotted</code> are for the variations.
+        </$.p>
+        <CodeBlock
+          $marginTop={"1rem"}
+          code={`.shaku-underline {
+  padding: 0 1ch;
+  position: relative;
+  display: block;
+  border-radius: 3px;
+  color: var(--color-shaku-underline-light, red);
+  margin: 0;
+  width: min-content;
+}
+
+.shaku-underline-line {
+  line-height: 0;
+  top: 0.5em;
+  position: absolute;
+  text-decoration-line: overline;
+  text-decoration-color: var(--color-shaku-underline-light, red);
+  color: transparent;
+  pointer-events: none;
+  user-select: none;
+  text-decoration-thickness: 2px;
+}
+
+.shaku-underline-wavy > .shaku-underline-line {
+  text-decoration-style: wavy;
+  top: 0.7em;
+}
+
+.shaku-underline-solid > .shaku-underline-line {
+  text-decoration-style: solid;
+}
+
+.shaku-underline-dotted > .shaku-underline-line {
+  text-decoration-style: dotted;
+}`}
+          lang="css"
+        />
+
         <$.h3>Highlight Lines</$.h3>
 
         <CodeBlock
@@ -206,7 +297,23 @@ function useSomeEffect({blog}) {
           lang="js"
         />
 
-        <$.h3>Highlight words</$.h3>
+        <$.p>
+          <code>.shaku .line.highlight</code> could be used to set highlight
+          style.
+        </$.p>
+        <CodeBlock
+          $marginTop={"1rem"}
+          code={`pre.shaku .line.highlight {
+  background-color: var(
+    --color-shaku-highlight-light,
+    color-mix(in srgb, rgb(5, 118, 149) 15%, #fff)
+  );
+  display: block;
+}`}
+          lang="css"
+        />
+
+        <$.h3>Highlight words(inline)</$.h3>
 
         <CodeBlock
           code={`//       (           )
@@ -238,6 +345,38 @@ function useSomeEffect({blog}) {
     }, [blog])
 }`}
           lang="js"
+        />
+        <$.p>
+          <code>.shaku-inline-highlight</code> is used to style the inline
+          blocks, target specific blocks with the id you set -{" "}
+          <code>[data-id=*]</code>.
+        </$.p>
+        <CodeBlock
+          $marginTop={"1rem"}
+          code={`.shaku-inline-highlight {
+  background-color: #05a4fa30;
+  border-bottom: 2px solid rgb(9, 113, 239);
+  margin: 0 1px;
+  border-radius: 3px;
+  padding: 0 3px;
+}
+
+.shaku-inline-highlight[data-id="1"] {
+  background-color: #05a4fa30;
+  border-bottom: 2px solid rgb(9, 113, 239);
+}
+
+.shaku-inline-highlight[data-id="2"] {
+  background-color: #fa05f230;
+  border-bottom: 2px solid rgb(235, 4, 158);
+}
+
+.shaku-inline-highlight[data-id="3"] {
+  background-color: #05faa930;
+  border-bottom: 2px solid green;
+}
+`}
+          lang="css"
         />
 
         <$.h3>Dim lines</$.h3>
@@ -278,6 +417,17 @@ useEffect(() => {
 }`}
           lang="js"
         />
+        <$.p>
+          Use <code>.dim</code> to style dimmed lines.
+        </$.p>
+        <CodeBlock
+          $marginTop={"1rem"}
+          code={`.shaku .line.dim {
+  opacity: 0.3;
+}`}
+          lang="css"
+        />
+
         <$.h3>Focus lines</$.h3>
 
         <CodeBlock
@@ -296,7 +446,8 @@ useEffect(() => {
           shakuEnabled
         />
         <$.p>
-          Focus means to highlight some lines and dim the others. Use{" "}
+          Focus means to highlight some lines and dim the others, it is a
+          shorthand of <code>@highlight</code> and <code>@dim</code>. Use{" "}
           <code>@focus</code> to focus next line, <code>@focus start</code> and
           <code>@focus end</code> for multiple lines.
         </$.p>
@@ -314,7 +465,9 @@ useEffect(() => {
 }`}
           lang="js"
         />
-
+        <$.p>
+          Since it is a shorthand syntax, there is no special class for it.
+        </$.p>
         <$.h3>Position Shift</$.h3>
 
         <CodeBlock
@@ -351,7 +504,7 @@ useEffect(() => {
         <$.hr $margin="2rem auto" $width={"50%"} />
         <p>
           Got an issue or have better ideas? Raise an issue on{" "}
-          <a href="https://github.com/JSerZANP/shaku">shaku@github</a>.
+          <a href="https://github.com/JSerZANP/shaku">shaku repo</a>.
         </p>
       </$.div>
     </div>
